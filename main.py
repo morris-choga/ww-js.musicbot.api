5faee63be4291146d75eae369bf231e406a06db8
morrischoga <mchoga@outlook.com>
2024-02-19 00:10:38 +0100

    Initial commit

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ app = Flask(__name__)
 def hello_world():
     requested_song = request.get_json()
     song_metadata = get_song_metadata(f"{requested_song['key']}")
+    print("Anything happening here?")
 
     song = download(song_metadata["title"],song_metadata["video_id"],os.path.join("/usr/src/api","songs"))
     tagger(song_metadata["title"],song_metadata["artist"],song_metadata["album_name"],song_metadata["url"],song)
 
     # return song
-    return song_metadata
+    return "Noise"
 
 @app.route('/get_status', methods=['GET'])
 def get_status():
