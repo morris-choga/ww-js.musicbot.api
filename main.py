51d00e5509a5d214a7e7fec37d0531872ad7cd14
morrischoga <mchoga@outlook.com>
2024-02-19 00:39:00 +0100

    Initial commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def hello_world():
     tagger(song_metadata["title"],song_metadata["artist"],song_metadata["album_name"],song_metadata["url"],song)
 
     # return song
-    return song_metadata
+    return song
 
 @app.route('/get_status', methods=['GET'])
 def get_status():
